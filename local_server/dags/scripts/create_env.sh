2209dc94a591c94de3fa11e26c52a79974b8e4a2
phandaiduonghcb <phandaiduonghcb@gmail.com>
2023-06-01 13:36:50 +0700

    Change repo url when creating env

--- local_server/dags/scripts/create_env.sh
+++ local_server/dags/scripts/create_env.sh
@@ -10,12 +10,12 @@ conda activate training_env
 conda env list
 conda install pip -n training_env
 cd $HOME/conda/envs/training_env
-git clone https://github.com/phandaiduonghcb/mlops
+git clone https://github.com/phandaiduonghcb/local_mlops
 # echo "Install dependencies..."
-$HOME/conda/envs/training_env/bin/pip install -r ./mlops/requirements-dev.txt --no-cache-dir
+$HOME/conda/envs/training_env/bin/pip install -r ./local_mlops/requirements-dev.txt --no-cache-dir
 $HOME/conda/envs/training_env/bin/pip cache purge
 pip cache purge
-cd mlops
+cd local_mlops
 echo "Current working dir: $PWD"
 dvc pull -AaR -r dvc-flower-bucket
 ls ./ml/data/*
